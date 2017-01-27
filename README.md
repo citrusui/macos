3177e87cc904f95701bfce9d0152bac005a2fbd5
Avery Magnotti <averymagnotti@outlook.com>
2017-01-27 09:35:56 -0500

    Update in sync with citrusui/setup@ffb5119

---
+++ README.md
@@ -0,0 +1,11 @@
+# macOS Preferences
+
+My personal setup for [macOS.](https://www.apple.com/macos/)
+
+## Install
+
+```sh
+git clone https://github.com/citrusui/macos && source macos/init.sh
+```
+
+Licensed under [MIT.](LICENSE.md)
---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function checkTools() {
+  if [ ! "$(which git)" ]; then
+    echo "Git is required." # TODO: install these automatically
+    echo "Please install them from your package manager."
+    echo "If you are on macOS, run xcode-select --install."
+    sleep 5;
+    exit 1;
+  fi
+}
+
+function pullSource() {
+  rsync --exclude ".git/" --exclude ".travis.yml" --exclude "init.sh" --exclude "*.md" -avh --no-perms . ~; # TODO: suppress output with -s and report if files were untouched
+}
+
+checkTools
+
+cd "$(dirname "${BASH_SOURCE}")";
+echo ""
+echo "Pulling updates..."
+echo ""
+git pull origin master;
+if [ "$1" == "-y" ]; then
+  pullSource
+else
+  echo ""
+  read -p "This may overwrite your existing preferences. Continue? [Y/N] "
+  echo ""
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    pullSource
+  fi
+fi
